e5ede1cd17c7819a361e830dcdcc372c0ca08998
Thomas B <braithwaite.th@gmail.com>
2022-11-12 01:43:22 -0300

    add websocket logger

--- handler.js
+++ handler.js
@@ -1,9 +1,16 @@
 const helper = require("./helpers");
+const fs = require('fs')
+let datetime = new Date();
 
 async function processMessage(message) {
     console.log(message)
     console.log(message.contenido)
     console.log(message.emisor)
+
+    fs.appendFile('websocket.log', datetime + '\n' + 'Emisor: ' + message.emisor + '\nContenido:\n' + message.contenido + '\n#######################\n', function (err) {
+        if (err) return console.log(err);
+        console.log('Appended!');
+     });
     await helper.connectMongo()
 
     //Actualizar db
