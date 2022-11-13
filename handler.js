e4fc0ef38e61f32884cbd59716ea63fc37f97e98
Thomas B <braithwaite.th@gmail.com>
2022-11-13 17:59:11 -0300

    Fix Datetime

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
 const helper = require("./helpers");
 const fs = require('fs')
-let datetime = new Date();
+
 
 async function processMessage(message) {
     console.log(message.contenido)
     console.log(message.emisor)
+
+    let datetime = new Date();
 
     fs.appendFile('websocket.log', datetime + '\n' + 'Emisor: ' + message.emisor + '\nContenido:\n' + message.contenido + '\n#######################\n', function (err) {
         if (err) return console.log(err);
